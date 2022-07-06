c536fd744969cd8b62627de90006096de735cc69
mr.sparks.x2 <mr.sparks.x2@gmail.com>
2022-07-06 22:04:33 +0500

    kyu 6

--- WhoLikesIt.js
+++ WhoLikesIt.js
@@ -1,18 +1,15 @@
 const likes = (names) => {
-    let namesLength = names.length;
-    if (namesLength === 1) {
-        return names[0] + ' ' + 'likes this';
-    }
-    if (namesLength === 2) {
-        return names[0] + ' and ' + names[1] + ' like this';
-    }
-    if (namesLength === 3) {
-        return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
-    }
-    if (namesLength > 3) {
-        return names[0] + ', ' + names[1] + ' and ' + (namesLength - 2) + ' others like this';
-    } else {
-        return 'no one likes this';
+    switch (names.length) {
+        case 0:
+            return 'no one likes this';
+        case 1:
+            return names[0] + ' ' + 'likes this';
+        case 2:
+            return names[0] + ' and ' + names[1] + ' like this';
+        case 3:
+            return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+        default:
+            return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
     }
 };
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
