62d931a57bc5e167e0b96d874fff77b093c374f1
mr.sparks.x2 <mr.sparks.x2@gmail.com>
2022-07-06 21:27:44 +0500

    List Filtering

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const likes = (names) => {
-    let namesLength = names.length;
-    if (namesLength === 1) {
-        return names[0] + ' ' + 'likes this';
-    }
-    if (namesLength === 2) {
-        return names[0] + ' and ' + names[1] + ' like this';
-    }
-    if (namesLength === 3) {
-        return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
-    }
-    if (namesLength > 3) {
-        return names[0] + ', ' + names[1] + ' and ' + (namesLength - 2) + ' others like this';
-    } else {
-        return 'no one likes this';
-    }
+const filter_list = (l) => {
+    return l.filter((item) => typeof item === 'number');
 };
