bab50b9c63bff9c4f090a87f4f9710bf1cc62f7b
EvelinSu <mr.sparks.x2@gmail.com>
2022-07-08 16:42:43 +0500

    6 kyu

---
+++ StopGninnipsMySdrow.js
@@ -0,0 +1,9 @@
+function spinWords(string) {
+    let result = [];
+    string
+        ? string.split(' ').forEach((el) => (el.length >= 5 ? result.push(el.split('').reverse().join('')) : result.push(el)))
+        : '';
+    return result.join(' ');
+}
+
+console.log(spinWords(''));
