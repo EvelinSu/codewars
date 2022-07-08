bb3f24356dd818eefd56aae9d4d12691bb2a3cf4
EvelinSu <mr.sparks.x2@gmail.com>
2022-07-08 15:59:32 +0500

    6 kyu

---
+++ ConvertStringToCamelCase.js
@@ -0,0 +1,6 @@
+function toCamelCase(str) {
+    let foo = (match) => match[1].toUpperCase();
+    return str.replace(/[-_]./g, foo);
+}
+
+console.log(toCamelCase('The-stealth_warrior')); // TheStealthWarrior
