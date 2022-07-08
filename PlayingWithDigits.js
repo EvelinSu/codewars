a4cbbbd93c65821d754a8c800772e6b490144839
mr.sparks.x2 <mr.sparks.x2@gmail.com>
2022-07-08 14:01:31 +0500

    6 kyu

---
+++ PlayingWithDigits.js
@@ -0,0 +1,9 @@
+function digPow(n, p) {
+    let result = 0;
+    String(n)
+        .split('')
+        .forEach((el) => (result += Math.pow(el, p++)));
+    return result / n >= 1 && Number.isInteger(result / n) ? result / n : -1;
+}
+
+console.log(digPow(462882, 3)); // 51, since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
