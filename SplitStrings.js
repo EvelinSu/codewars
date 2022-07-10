da9f6e924fafff448c278504eada86357de0bb0d
EvelinSu <mr.sparks.x2@gmail.com>
2022-07-10 16:05:57 +0500

    6 kyu

---
+++ SplitStrings.js
@@ -0,0 +1,15 @@
+function solution(str) {
+    let result = [];
+    str = str.length % 2 === 0 ? str : str + '_';
+    for (let i = 0; i < str.length; i++) {
+        result.push(
+            str
+                .split('')
+                .slice(i++, i + 1)
+                .join(''),
+        );
+    }
+    return result;
+}
+
+console.log(solution('abcdefg')); // ['ab', 'cd', 'ef', 'g_']
