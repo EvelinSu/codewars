bbc61baf19617903c1dffe17cc8de32577a00c44
EvelinSu <mr.sparks.x2@gmail.com>
2022-07-10 21:57:06 +0500

    5 kyu

---
+++ PeteTheBaker.js
@@ -0,0 +1,14 @@
+recipe = { flour: 500, sugar: 200, eggs: 1 };
+available = { flour: 1500, sugar: 1200, eggs: 3, milk: 200 };
+
+function cakes(recipe, available) {
+    let result = [];
+    for (let key of Object.keys(recipe)) {
+        result.push(available[key] / recipe[key]);
+        if (!available[key]) {
+            return 0;
+        }
+    }
+    return Math.floor(Math.min.apply(null, result));
+}
+console.log(cakes(recipe, available)); // 3
