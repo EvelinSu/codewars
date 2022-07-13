a800e5b876a19cbac939b01a63187f9fce82a96b
EvelinSu <firest-evi@mail.ru>
2022-07-13 14:37:27 +0500

    6 kyu

---
+++ CountingDuplicates.js
@@ -0,0 +1,13 @@
+function duplicateCount(text) {
+    let result = 0;
+    let charCount = {};
+    text.toLowerCase()
+        .split('')
+        .forEach(function (char) {
+            charCount[char] = (charCount[char] || 0) + 1;
+        });
+    Object.values(charCount).forEach((el) => el > 1 && result++);
+    return result;
+}
+
+console.log(duplicateCount('Indivisibilities')); // 2
